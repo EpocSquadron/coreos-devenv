e069f8955199b9fafd7d8cddfa23f3966f5ca1c5
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2013-11-21 11:50:27 -0500

    Fix bugs and improve coloring of the install script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,40 +8,46 @@ runChecks() {
 
 	# Check dependencies
 	hash git 2>&- || {
-		echo >&2 ":: Git is required but is either not installed or not in your PATH.  Aborting."
+		echo -e >&2 "\033[0;31mGit is required but is either not installed or not in your PATH.  Aborting.\033[0m"
 		exit 1
 	}
 
 	# Check we haven't already installed
 	if [ -d "$HIDDEN_MASTER_DIRECTORY" ]; then
-		echo >&2 ":: You already have an instance of '$HIDDEN_MASTER_DIRECTORY' in this folder. If you need to continue, remove it first."
-		exit 1
+		echo -e >&2 "\033[0;31mYou already have an instance of '$HIDDEN_MASTER_DIRECTORY' in this folder. If you need to continue, remove it first.\033[0m"
+		exit 2
+	fi
+
+	# Don't want to run this if they have
+	# a pre-existing vagrantfile.
+	if [ -f "Vagrantfile" ]; then
+		echo -e >&2 "\033[0;31mYou have a Vagrantfile in this directory, either run 'vagrant destroy' and move the file out of the way or choose another directory.\033[0m"
+		exit 3
 	fi
 
 }
 
 createMasterDirectory() {
 
-	echo ":: Creating hidden directory at '$HIDDEN_MASTER_DIRECTORY'."
+	echo -e "\033[0;32m:: Cloning to hidden directory at '$HIDDEN_MASTER_DIRECTORY'.\033[0m"
 	git clone https://github.com/EpocSquadron/coreos-devenv.git "$HIDDEN_MASTER_DIRECTORY"
 
 }
 
 copyVagrantfile() {
 
-	echo ":: Copying the Vagrantfile to the current directory."
-	cp "$THIS_DIR/../Vagrantfile" .
+	echo -e "\033[0;32m:: Copying the Vagrantfile to the current directory.\033[0m"
+	cp "$HIDDEN_MASTER_DIRECTORY/Vagrantfile" .
 
 }
 
 bootstrapDatabaseDirectory() {
 
 	if [ -d "$HIDDEN_DATABASE_DIRECTORY" ]; then
-		echo ":: You already have an instance of '$HIDDEN_DATABASE_DIRECTORY' in this folder."
-		echo -n "   Would you like to overwrite the data? [y/N] "
+		echo -n -e "\033[0;33m:: You already have an instance of '$HIDDEN_DATABASE_DIRECTORY' in this folder. Would you like to overwrite the data? [y/N]\033[0m "
 		read RESPONSE
 
-		if [ "$RESPONSE" == 'y' || "$RESPONSE" == 'Y' || "$RESPONSE" == 'yes' ]; then
+		if [[ "$RESPONSE" == 'y' || "$RESPONSE" == 'Y' || "$RESPONSE" == 'yes' ]]; then
 			clearDatabases
 			copyBlankDatabases
 		fi
@@ -54,26 +60,30 @@ bootstrapDatabaseDirectory() {
 
 clearDatabases() {
 
-	mv "$HIDDEN_DATABASE_DIRECTORY/mysql" "$HIDDEN_DATABASE_DIRECTORY/mysql.old-"
+	local TIMESTAMP=`date +"%Y-%m-%d-%H%M%S"`
+
+	if [ -d "$HIDDEN_DATABASE_DIRECTORY/mysql" ]; then
+		mv "$HIDDEN_DATABASE_DIRECTORY/mysql" "$HIDDEN_DATABASE_DIRECTORY/mysql.old-$TIMESTAMP"
+	fi
 
 }
 
 copyBlankDatabases() {
 
-	echo ":: Creating hidden directory at '$HIDDEN_DATABASE_DIRECTORY' and populating it with blank databases."
-	mkdir "$HIDDEN_DATABASE_DIRECTORY" && \
+	echo -e "\033[0;32m:: Creating hidden directory at '$HIDDEN_DATABASE_DIRECTORY' and populating it with blank databases.\033[0m"
+	mkdir -p "$HIDDEN_DATABASE_DIRECTORY" && \
 	cp -r "$HIDDEN_MASTER_DIRECTORY/database/mysql" "$HIDDEN_DATABASE_DIRECTORY/"
 
 }
 
 offerDnsmasq() {
 
-	if [[ "$OSTYPE" ~= "darwin" ]]; then
+	if [[ "$OSTYPE" =~ "darwin" ]]; then
 
-		echo -n ":: Would you like to install dnsmasq for wildcard DNS to the development environment? [Y/n] "
+		echo -n -e "\033[0;33m:: Would you like to install dnsmasq for wildcard DNS to the development environment? [Y/n]\033[0m "
 		read RESPONSE
 
-		if [ "$RESPONSE" == "" || "$RESPONSE" == 'y' || "$RESPONSE" == 'Y' || "$RESPONSE" == 'yes' ]; then
+		if [[ "$RESPONSE" == "" || "$RESPONSE" == 'y' || "$RESPONSE" == 'Y' || "$RESPONSE" == 'yes' ]]; then
 			source "$HIDDEN_MASTER_DIRECTORY/scripts/osx/install-dnsmasq.sh"
 		fi
 
@@ -89,6 +99,9 @@ main() {
 	bootstrapDatabaseDirectory && \
 	offerDnsmasq
 
+	echo -e "\033[0;32m:: Done installing.\033[0m"
+
 }
 
+main
 
